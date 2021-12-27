b3dbfc8b6efec3ba6e55832e757e2aba3abe35eb
Kyle D'Arcy <kyle.darcy@icloud.com>
2021-12-27 17:04:29 -0500

    2 temp sensors working wirelessly

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,11 +1,4 @@
-/*
-* Arduino Wireless Communication Tutorial
-*       Example 1 - Receiver Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-*/
+
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
@@ -20,19 +13,26 @@ uint64_t address[6] = {
   0xB3B4B5B60FLL,
   0xB3B4B5B605LL
 };
-float data[3];
+
+struct PayloadStruct {
+  char nodeID;
+  float tempurature;
+  float humidity;
+  float heat_idx;
+};
+PayloadStruct payload;
 
 void setup() {
   Serial.begin(9600);
   radio.begin();
   radio.openReadingPipe(0, address[0]);
   radio.openReadingPipe(1, address[1]);
-  radio.setPALevel(RF24_PA_MIN);
+  radio.setPALevel(RF24_PA_MAX);
   radio.startListening();
 }
 void loop() {
   if (radio.available()) {
-    radio.read(&data, sizeof(data));
+    radio.read(&payload, sizeof(payload));
 //    Serial.print("Temp: ");
 //    Serial.print(data[0]);
 //    Serial.write(0xC2);  //send degree symbol
@@ -46,6 +46,10 @@ void loop() {
 //    Serial.write(0xC2);  //send degree symbol
 //    Serial.write(0xB0);  //send degree symbol
 //    Serial.println("F   ");
-      Serial.println(data[0]);
+
+      Serial.print("Node ID ");
+      Serial.print((int) payload.nodeID);
+      Serial.print(" Data: ");
+      Serial.println(payload.tempurature);
   }
 }
---
+++ transmitter0/transmitter0.ino
@@ -0,0 +1,48 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT11   
+
+char role = 0;
+
+uint64_t address[6] = {
+  0x7878787878LL,
+  0xB3B4B5B6F1LL,
+  0xB3B4B5B6CDLL,
+  0xB3B4B5B6A3LL,
+  0xB3B4B5B60FLL,
+  0xB3B4B5B605LL
+};
+
+struct PayloadStruct {
+  char nodeID;
+  float tempurature;
+  float humidity;
+  float heat_idx;
+};
+PayloadStruct payload;
+
+RF24 radio(7, 8); // CE, CSN
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(address[role]);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.stopListening();
+
+  dht.begin();
+  payload.nodeID = role;
+}
+
+void loop() {
+  payload.tempurature = dht.readTemperature(true); 
+  payload.humidity = dht.readHumidity();  
+  payload.heat_idx = dht.computeHeatIndex(payload.tempurature, payload.humidity);
+
+  radio.write(&payload, sizeof(payload));
+  delay(1000);
+}
